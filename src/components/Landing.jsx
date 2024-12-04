0bf6cf39f294f061084ca581f340e9638350bba6
unknown <goodlucketim94@gmail.com>
2024-12-04 20:17:47 +0100

    overhaul

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,7 +4,7 @@ import vote from "../assets/images/rb_4448.png"
 const LandingPage = () => {
     return ( 
         <header className="md:py-2 md:w-11/12 mx-auto">
-            <main className="md:grid md:grid-cols-2 items-center h-screen md:h-92 overflow-hidden mx-auto py-10">
+            <main className="md:grid md:grid-cols-2 items-center h-screen md:h-92 overflow-hidden mx-auto pt-10">
                 <div className="text md:relative md:text-left p-4 rounded-xl mb-5">
                     <p className="p-2 md:text-2xl text-green-700">Your Vote... Your Power...</p> 
                     <h1 className="font-bold px-2 text-2xl md:text-3xl mb-2">
@@ -12,7 +12,9 @@ const LandingPage = () => {
                         <br />
                         VOTE WITH  <span className="px-2 text-green-700 lg:text-4xl md:text-3xl rounded-md ">CONFIDENCE</span>
                     </h1>
-                    <p className="text-slate-900 p-2 md:text-xl">Simple, Secure and Transparent Voting starts with<Link className="text-green-700 text-xl font-bold underline rounded-lg shadow-md" to="/authentication"> iVote</Link></p>
+                    <p className="text-slate-900 p-2 md:text-xl mb-5">Simple, Secure and Transparent Voting starts with<Link className="text-green-700 text-xl font-bold underline rounded-lg shadow-md" to="/authentication"> iVote</Link></p>
+                    
+                    <Link className="bg-green-700 rounded-md shadow-md text-white px-4 py-2" to="/authentication"> <button>Get Started</button></Link>
                 </div>
                 <div className="image">
                     <img src={vote} alt="E-Voting" className="h-72 object-cover" />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
 import { Link as ScrollLink } from "react-scroll";
 import { Link as RouterLink } from "react-router-dom";
+import inec from '../assets/images/inec-logo.jpg'
+
 const Navbar = () => {
     const [navMenu, setNavMenu] = useState(false)
     const location = useLocation()
@@ -30,8 +32,9 @@ const Navbar = () => {
     return ( 
         <header className="bg-green-700 sticky top-0 left-0 z-10 shadow-lg">
             <nav ref={navMenuRef} className="relative p-5 md:p-3 flex items-center justify-between w-11/12 mx-auto text-white font-sans">
-                <div className="logo">
-                    <h2 className="font-bold text-2xl"><ScrollLink to="/" >iVOTE</ScrollLink></h2>
+                <div className="logo flex items-center  md:w-2/12">
+                    <img src={inec} alt="INEC logo" className="rounded-full w-2/12 md:w-3/12" /> &nbsp;
+                    <h2 className="font-bold text-2xl"><RouterLink to="/" >iVOTE</RouterLink></h2>
                 </div>
                 <div className={`${navMenu ? ('block'): ('hidden')} actions bg-green-700 p-10 absolute top-10 -right-7 h-screen md:h-0 md:top-0 md:right-0 md:bg-transparent md:p-5 md:text-center md:relative md:flex items-center gap-5`}>
                     <li className="p-2 my-2 list-none"><RouterLink to="/">Home</RouterLink></li>
