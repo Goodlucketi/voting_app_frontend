04635806ef2b9f56526a6898e130ac8d19f8e52d
unknown <goodlucketim94@gmail.com>
2024-12-05 14:10:49 +0100

    overhaul

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,19 @@
 
-import aboutImg3 from "../assets/images/E-voting.png"
+import aboutImg3 from "../assets/images/about.png"
 import { Link } from "react-router-dom";
 const About = () => {
     
     return ( 
         <main className="w-11/12 mx-auto shadow-md px-4">
-            <div className="about grid md:grid-cols-2 gap-x-5">
+            <div className="about grid md:grid-cols-2 gap-x-5 relative overflow-hidden">
+                <h2 className="font-bold text-3xl text-green-700 mb-5 ">About iVOTE</h2>
+
                 <div className="img-grid">
                     <div className="img1">
-                        <img src={aboutImg3} alt="" />
+                        <img src={aboutImg3} alt="" className="-rotate-[17deg] w-full md:w-4/12 md:absolute -bottom-10" />
                     </div>
                 </div>
                 <div className="about-text px-4 py-6">
-                    <h2 className="font-bold text-3xl text-green-700 mb-5">About iVOTE</h2>
                     <p className="text-lg mb-7">
                     iVOTE is a modern voting platform designed to revolutionize the electoral process of the Independent National Electoral Commission (INEC) of Nigeria with security, transparency, and simplicity at its core. Built for electoral bodies and voters alike, our platform ensures every vote is counted accurately and securely while empowering stakeholders to monitor and manage elections seamlessly.
                     </p>
--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -2,9 +2,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 const Feature = ({title, description, icon}) => {
     return ( 
         <main>
-            <div className="feature text-slate-50 flex flex-col items center shadow-md px-6 py-12 h-60 relative border rounded-lg">
+            <div className="feature text-slate-50 flex flex-col items center shadow-md px-6 py-8 md:py-12 h-60 relative border rounded-lg">
                 <FontAwesomeIcon icon={icon} className="text-4xl mb-5 absolute -top-6 left-10 " />
-                <h3 className="font-bold text-2xl mb-5">{title}</h3>
+                <h3 className="font-bold text-2xl mb-3">{title}</h3>
                 <p className="text-lg">{description}</p>
             </div>
         </main>
