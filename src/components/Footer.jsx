88f704ee08ccf01c7bff6688de502952a36e62f3
unknown <goodlucketim94@gmail.com>
2024-12-05 14:31:20 +0100

    overhaul

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,36 +2,36 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
     return ( 
-        <footer className="bg-green-100/75 pt-10">
-            <div className="w-11/12 mx-auto px-4">
-                <div className="p-2 grid grid-cols-2 md:grid-cols-4 gap-5 md:gap-10 md:text-lg">
+        <footer className="bg-green-100/75 pt-10    ">
+            <div className="w-11/12 mx-auto px-4 md:py-8">
+                <div className="p-2 grid grid-cols-2 md:grid-cols-4 gap-10 md:gap-64 md:text-lg">
                     <div className="about">
-                        <h3 className="font-bold">About</h3>
-                        <p><Link>About Us</Link></p>
-                        <p><Link>Testimonies</Link></p>
-                        <p><Link>Contact Us</Link></p>
-                        <p><Link>FAQ</Link></p>
+                        <h3 className="font-bold mb-2 text-xl">About</h3>
+                        <p className="mb-2"><Link>About Us</Link></p>
+                        <p className="mb-2"><Link>Testimonies</Link></p>
+                        <p className="mb-2"><Link>Contact Us</Link></p>
+                        <p className="mb-2"><Link>FAQ</Link></p>
                     </div>
 
                     <div className="services">
-                        <h3 className="font-bold">Services</h3>
-                        <p><Link>Voting</Link></p>
-                        <p><Link>Online Surveys</Link></p>
-                        <p><Link>Complaints</Link></p>
+                        <h3 className="font-bold mb-2 text-xl">Services</h3>
+                        <p className="mb-2"><Link>Voting</Link></p>
+                        <p className="mb-2"><Link>Online Surveys</Link></p>
+                        <p className="mb-2"><Link>Complaints</Link></p>
                     </div>
 
                     <div className="account">
-                        <h3 className="font-bold">My Account</h3>
-                        <p><Link>Authenticate</Link></p>
-                        <p><Link>Live Chat</Link></p>
-                        <p><Link>Support</Link></p>
+                        <h3 className="font-bold mb-2 text-xl">My Account</h3>
+                        <p className="mb-2"><Link>Authenticate</Link></p>
+                        <p className="mb-2"><Link>Live Chat</Link></p>
+                        <p className="mb-2"><Link>Support</Link></p>
                     </div>
 
                     <div className="legal">
-                        <h3 className="font-bold">Legal</h3>
-                        <p><Link>Security</Link></p>
-                        <p><Link>Terms of Service</Link></p>
-                        <p><Link>Privacy Policy</Link></p>
+                        <h3 className="font-bold mb-2 text-xl">Legal</h3>
+                        <p className="mb-2"><Link>Security</Link></p>
+                        <p className="mb-2"><Link>Terms of Service</Link></p>
+                        <p className="mb-2"><Link>Privacy Policy</Link></p>
                     </div>
                 </div>
             </div>
