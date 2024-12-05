c660fa77875153de6b7daa153317142ec3433c3d
unknown <goodlucketim94@gmail.com>
2024-12-05 14:19:41 +0100

    overhaul

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -12,14 +12,14 @@ const Accordion = ({faqs})=>{
         <main className="p-4">
             {faqs.map((faq, index)=>(
                 <div key={index} className="shadow-md p-4">
-                    <div className="question flex justify-between" onClick={()=>toggleAccordion(index)}>
-                        <h3>{faq.question}</h3>
+                    <div className="question flex justify-between p-4 mb-3" onClick={()=>toggleAccordion(index)}>
+                        <h3 className="md:text-lg">{faq.question}</h3>
                         <button>                    
                             {activeIndex==index ?<FontAwesomeIcon icon={faMinus} className="text-sm" /> : <FontAwesomeIcon icon={faPlus} className="text-sm" /> }
                         </button>
                     </div>
                     {activeIndex==index && (
-                        <div className="answer p-4">
+                        <div className="answer p-2 md:text-lg">
                             {faq.answer}
                         </div>
                     )}
